2f1305a427abd7120d7af6ed32945d61c8a116de
Esmee29 <Esmee.Fulcher@netmatters-scs.com>
2024-02-07 15:21:12 +0000

    Cookies

--- js/cookies.js
+++ js/cookies.js
@@ -1,27 +1,50 @@
-// JavaScript Cookies //
+//Javascript Cookies
+const btnAcceptCookies = document.querySelector(".accept-cookies-btn");
+const btnManageConsent = document.querySelector(".cookie-settings-btn");
+const cookieSection = document.getElementById("cookie-consent");
+const body = document.body;
+let testDate = new Date();
+console.log(testDate);
 
-const btnAcceptCookies = document.getElementsByClassName("accept-cookies-btn"); // Get the accept cookies button element
-const btnManageConsent = document.getElementsByClassName("cookie-settings-btn"); // Get the manage consent button element
-const cookieSection = document.getElementById("cookie-consent"); // Get the cookie section element
-const body = document.querySelector("body");// Get the body element
-
-// Check if the user has consented to the use of cookies
-if (!localStorage.getItem("cookieConsent")) {
-  // Display the cookie consent popup
-  cookieSection.style.display = "block";
-
-  // Add event listener to the accept cookies button
-  btnAcceptCookies.addEventListener("click", function() {
-    // Store the user's consent locally
-    localStorage.setItem("cookieConsent", true);
-
-    // Hide the cookie consent popup
+btnAcceptCookies.addEventListener("click", () => {
     cookieSection.style.display = "none";
-  });
+    setCookie("cookie", true, 1); // number is days
+    
+    body.style.overflow = "auto";
+    console.log("cookie should be stored now");
+});
+
+btnManageConsent.addEventListener("click", () => {
+    cookieSection.style.display = "flex";
+    body.style.overflow = "hidden";
+});
+
+function setCookie(name, value, duration) {
+    let date = new Date();
+    date.setTime(date.getTime() + (duration * 24 * 60 * 60 * 1000)); // sets the numeric value inputted into 'duration' in days
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + "; path=/";
 }
 
-// Add event listener to the manage consent button
-btnManageConsent.addEventListener("click", function() {
-  // Display the cookie consent popup
-  cookieSection.style.display = "block";
-});
+function getCookie(name) {
+    const cName = name + "=";
+    const cDecoded = decodeURIComponent(document.cookie);
+    const cArray = cDecoded.split("; ");
+    let value;
+    
+    cArray.forEach(val => {
+        if (val.indexOf(name) === 0) value = val.substring(name.length);
+    });
+
+    return value;
+}
+
+function cookieMessage() {
+    if (!getCookie("cookie")) {
+        console.log("cookie not here");
+        cookieSection.style.display = "flex";
+        body.style.overflow = "hidden";
+    }
+}
+
+window.addEventListener("load", cookieMessage);

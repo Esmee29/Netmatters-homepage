f851b1d1c92c24ceb998596bda44a857be014711
Esmee29 <Esmee.Fulcher@netmatters-scs.com>
2024-02-12 16:39:53 +0000

    Banner Carousel

---
+++ js/carousel.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+    // Header Banner Slider using Slick
+    $('.banner-carousel-container').slick({
+        autoplay: true,
+        dots: true,
+        nextArrow: false,
+        prevArrow: false,
+    });
+});
--- js/sticky-header.js
+++ js/sticky-header.js
@@ -1,9 +1,15 @@
 // ==========================================================================
 // JavaScript Sticky Header
 // ==========================================================================
-const stickyEle = document.querySelector("#sticky");
-const stickyEleHeight = stickyEle.offsetHeight;
+// Add the sticky-hide class to the sticky header on page load
+window.onload = function() {
+    const stickyHeader = document.querySelector("#sticky");
+    stickyHeader.classList.add("sticky-hide");
+};
 
+// Sticky header logic
+const stickyHeader = document.querySelector("#sticky");
+const stickyHeaderHeight = stickyHeader.offsetHeight;
 
 let prevScrollPos = window.pageYOffset;
 
@@ -12,29 +18,28 @@ window.onscroll = function() {stickyHeaderScroll()};
 function stickyHeaderScroll() {
     let currentScrollPos = window.pageYOffset;
 
-    if (currentScrollPos > prevScrollPos && currentScrollPos > stickyEleHeight) // user is scrolling down and is past the default header at the top of page
+    if (currentScrollPos > prevScrollPos && currentScrollPos > stickyHeaderHeight) // user is scrolling down and is past the default header at the top of page
     {
-        stickyEle.classList.add("sticky-hide");
-        stickyEle.classList.remove("sticky-show");
+        stickyHeader.classList.add("sticky-hide");
+        stickyHeader.classList.remove("sticky-show");
     }
     else // user is scrolling upwards
     {
-        stickyEle.classList.add("sticky-show");     
-        stickyEle.classList.remove("sticky-hide");
+        stickyHeader.classList.add("sticky-show");     
+        stickyHeader.classList.remove("sticky-hide");
     }
 
     if (currentScrollPos == 0) // removes sticky header at the top of the page, adds it back in if the webpage is scrolled past the height of the header
     {
-        stickyEle.classList.add("sticky-remove");
-        stickyEle.classList.remove("sticky-add");
+        stickyHeader.classList.add("sticky-remove");
+        stickyHeader.classList.remove("sticky-add");
         console.log("sticky removed at top of page");
     }
-    else if (currentScrollPos > stickyEleHeight)
+    else if (currentScrollPos > stickyHeaderHeight)
     {
-        stickyEle.classList.add("sticky-add");
-        stickyEle.classList.remove("sticky-remove");
+        stickyHeader.classList.add("sticky-add");
+        stickyHeader.classList.remove("sticky-remove");
     }
 
     prevScrollPos = currentScrollPos;
 }
-

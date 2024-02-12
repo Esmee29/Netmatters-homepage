2ac0837f05cf00075db1038eb66a5d5c6d2afc57
Esmee29 <Esmee.Fulcher@netmatters-scs.com>
2024-02-12 08:52:26 +0000

    Sticky hamburgers
    hamburgers in sticky header to work with sidebar

--- js/sidebar.js
+++ js/sidebar.js
@@ -7,9 +7,13 @@ const btnHamburgerSmall = document.querySelector(".hamburger-small");
 const sideBar = document.querySelector(".sidebar");
 const main = document.querySelector(".main");
 const darkOverlay = document.querySelector(".dark-overlay");
+const btnHamburgerSticky = document.querySelector(".hamburger-sticky");
+const btnHamburgerSmallSticky = document.querySelector(".hamburger-small-sticky");
 
 btnHamburger.addEventListener("click", toggleSidebar);
 btnHamburgerSmall.addEventListener("click", toggleSidebar);
+btnHamburgerSticky.addEventListener("click", toggleSidebar);
+btnHamburgerSmallSticky.addEventListener("click", toggleSidebar);
 
 function toggleSidebar() {
     darkOverlay.classList.toggle("dark-overlay-active");
@@ -19,6 +23,10 @@ function toggleSidebar() {
     btnHamburger.classList.toggle("hamburger-spin-backwards");
     btnHamburgerSmall.classList.toggle("hamburger-spin");
     btnHamburgerSmall.classList.toggle("hamburger-spin-backwards");
+    btnHamburgerSticky.classList.toggle("hamburger-spin");
+    btnHamburgerSticky.classList.toggle("hamburger-spin-backwards");
+    btnHamburgerSmallSticky.classList.toggle("hamburger-spin");
+    btnHamburgerSmallSticky.classList.toggle("hamburger-spin-backwards");
     document.body.style.overflow = sideBar.classList.contains("sidebar-active") ? "hidden" : "auto";
 }
 
@@ -30,6 +38,10 @@ darkOverlay.addEventListener("click", () => {
     btnHamburger.classList.remove("hamburger-spin-backwards");
     btnHamburgerSmall.classList.remove("hamburger-spin");
     btnHamburgerSmall.classList.remove("hamburger-spin-backwards");
+    btnHamburgerSticky.classList.remove("hamburger-spin");
+    btnHamburgerSticky.classList.remove("hamburger-spin-backwards");
+    btnHamburgerSmallSticky.classList.remove("hamburger-spin");
+    btnHamburgerSmallSticky.classList.remove("hamburger-spin-backwards");
     document.body.style.overflow = "auto";
 });
 

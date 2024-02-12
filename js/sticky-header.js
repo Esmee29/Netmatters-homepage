8acbd59b828f859315f9268cb1e2d8db7cfe0ec8
Esmee29 <Esmee.Fulcher@netmatters-scs.com>
2024-02-12 08:43:13 +0000

    Sticky fixed

--- js/sticky-header.js
+++ js/sticky-header.js
@@ -4,42 +4,37 @@
 const stickyEle = document.querySelector("#sticky");
 const stickyEleHeight = stickyEle.offsetHeight;
 
-let prevScrollPos = window.pageYOffset;
 
-window.addEventListener('DOMContentLoaded', function() {
-    stickyHeaderScroll();
-});
+let prevScrollPos = window.pageYOffset;
 
 window.onscroll = function() {stickyHeaderScroll()};
 
 function stickyHeaderScroll() {
     let currentScrollPos = window.pageYOffset;
 
-    if (currentScrollPos > prevScrollPos && currentScrollPos > stickyEleHeight) {
+    if (currentScrollPos > prevScrollPos && currentScrollPos > stickyEleHeight) // user is scrolling down and is past the default header at the top of page
+    {
         stickyEle.classList.add("sticky-hide");
         stickyEle.classList.remove("sticky-show");
-    } else {
+    }
+    else // user is scrolling upwards
+    {
         stickyEle.classList.add("sticky-show");     
         stickyEle.classList.remove("sticky-hide");
     }
 
-    if (currentScrollPos === 0) {
+    if (currentScrollPos == 0) // removes sticky header at the top of the page, adds it back in if the webpage is scrolled past the height of the header
+    {
         stickyEle.classList.add("sticky-remove");
         stickyEle.classList.remove("sticky-add");
         console.log("sticky removed at top of page");
-    } else if (currentScrollPos > stickyEleHeight) {
+    }
+    else if (currentScrollPos > stickyEleHeight)
+    {
         stickyEle.classList.add("sticky-add");
         stickyEle.classList.remove("sticky-remove");
     }
 
-    // New condition to hide #sticky when at the top of the page
-    if (currentScrollPos === 0) {
-        stickyEle.classList.add("sticky-hidden");
-    } else {
-        stickyEle.classList.remove("sticky-hidden");
-    }
-
     prevScrollPos = currentScrollPos;
 }
-
 

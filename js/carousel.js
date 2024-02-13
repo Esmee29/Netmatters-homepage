cd5638e72edea5c41c9e72eb0565c01c4822efab
Esmee29 <Esmee.Fulcher@netmatters-scs.com>
2024-02-13 11:40:25 +0000

    partner slideshow

--- js/carousel.js
+++ js/carousel.js
@@ -7,4 +7,26 @@ $(document).ready(function(){
         nextArrow: false,
         prevArrow: false,
     });
+    // Partner Banner Slider using Slick
+    $('.partner-slideshow').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        variableWidth: true,
+        prevArrow: false,
+        nextArrow: false,
+        draggable: false,
+        swipe: false
+    });
+    // Client Banner Slider using Slick
+    $('.affiliation-banner').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: false,
+        infinite: true,
+        variableWidth: true,
+        prevArrow: false,
+        nextArrow: false,
+        draggable: false,
+        swipe: false,
+      });
 });
